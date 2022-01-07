38536ddb87957a85ae2af277ef126017e9f2a7e2
EllieYY <xuanh_w@sina.com>
2022-01-07 15:54:01 +0800

    V00011-01 open OCT.

--- ticket/TicketReregisterWidget.cpp
+++ ticket/TicketReregisterWidget.cpp
@@ -78,17 +78,17 @@ bool TicketReregisterWidget::showData()
     }
 
     // TODO:确认什么时候关闭限制
-    // 限制一卡通的使用
-    if (info->icType() == OCT_CARD) {
-        ui->selectBtn2->setDisabled(true);
-        ui->selectBtn3->setDisabled(true);
+//    // 限制一卡通的使用
+//    if (info->icType() == OCT_CARD) {
+//        ui->selectBtn2->setDisabled(true);
+//        ui->selectBtn3->setDisabled(true);
 
-        ui->textTips->setText("无法操作洪城一卡通，请联系人工处理。");
-        ui->cashPollBtn->setDisabled(true);
-        ui->tUpdateBtn->setDisabled(true);
+//        ui->textTips->setText("无法操作洪城一卡通，请联系人工处理。");
+//        ui->cashPollBtn->setDisabled(true);
+//        ui->tUpdateBtn->setDisabled(true);
 
-        return true;
-    }
+//        return true;
+//    }
 
     // 进站时间 | 出站时间 | 更新原因 | 应收费用
     ui->lineEdit1->setText("");
